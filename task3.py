1ced29d2d607a55b032810963b07374d81d06b2a
Kunal Dubey <kunaldubey2297@gmail.com>
2021-11-10 02:06:54 +0530

    completed task3

--- task3.py
+++ task3.py
@@ -51,10 +51,20 @@ def isAreaNumber(phoneNumber):
     else:
         return False
 
-areaCodes = set() 
+areaCodes = set()
+bangloreCalls = 0
+
 
 for entry in calls: 
-    if(isAreaNumber(entry[0])):
-        areaCodes.add(entry[0][1:4])  
-        
-print(areaCodes)
+    if(isAreaNumber(entry[0])): 
+        areaCode = entry[0][1:4]
+        if(areaCode=='080'):
+            bangloreCalls+=1
+        areaCodes.add(entry[0][1:4])     
+
+percentage = bangloreCalls*100/(len(calls))
+
+for code in sorted(areaCodes,key=str):
+    print(code,end="\n") 
+
+print("%.2f percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore."%percentage)
